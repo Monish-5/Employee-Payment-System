888fb92738be5063373a0e8034ce4efaa5dbd349
Monish <164910588+Monish-5@users.noreply.github.com>
2024-07-13 14:17:35 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,95 @@
+package org.carproject;
+
+
+	import java.util.ArrayList;
+	import java.util.List;
+
+	abstract class Employee {
+	    private String name;
+	    private int id;
+
+	    public Employee(String name, int id) {
+	        this.name = name;
+	        this.id = id;
+	    }
+
+	    public String getName() {
+	        return name;
+	    }
+
+	    public int getId() {
+	        return id;
+	    }
+
+	    // Abstract method to be implemented by subclasses
+	    public abstract double calculateSalary();
+
+	    @Override
+	    public String toString() {
+	        return "Employee [name=" + name + ", id=" + id + ", salary=" + calculateSalary() + "]";
+	    }
+	}
+
+	class FullTimeEmployee extends Employee {
+	    private double monthlySalary;
+
+	    public FullTimeEmployee(String name, int id, double monthlySalary) {
+	        super(name, id);
+	        this.monthlySalary = monthlySalary;
+	    }
+
+	    @Override
+	    public double calculateSalary() {
+	        return monthlySalary;
+	    }
+	}
+
+	class PartTimeEmployee extends Employee {
+	    private int hoursWorked;
+	    private double hourlyRate;
+
+	    public PartTimeEmployee(String name, int id, int hoursWorked, double hourlyRate) {
+	        super(name, id);
+	        this.hoursWorked = hoursWorked;
+	        this.hourlyRate = hourlyRate;
+	    }
+
+	    @Override
+	    public double calculateSalary() {
+	        return hoursWorked * hourlyRate;
+	    }
+	}
+
+	class PayrollSystem {
+	    private List<Employee> employeeList;
+
+	    public PayrollSystem() {
+	        employeeList = new ArrayList<>();
+	    }
+
+	    public void addEmployee(Employee employee) {
+	        employeeList.add(employee);
+	    }
+
+	    public void removeEmployee(int id) {
+	        Employee employeeToRemove = null;
+	        for (Employee employee : employeeList) {
+	            if (employee.getId() == id) {
+	                employeeToRemove = employee;
+	                break;
+	            }
+	        }
+	        if (employeeToRemove != null) {
+	            employeeList.remove(employeeToRemove);
+	        }
+	    }
+
+	    public void displayEmployees() {
+	        for (Employee employee : employeeList) {
+	            System.out.println(employee);
+	        }
+	    }
+	}
+
+	
+
